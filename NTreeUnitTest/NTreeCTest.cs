0a5cd747dde8670aacf1b984f5f02dc1c6e350ce
Ludwig Zhao <luxwig@gmail.com>
2020-01-29 23:27:37 -0500

    Complete NTree

---
+++ NTreeUnitTest/NTreeCTest.cs
@@ -0,0 +1,442 @@
+using System;
+using Xunit;
+using Xunit.Abstractions;
+using MetroMate;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace NTreeUnitTest
+{
+    public class NTreeTest
+    {
+
+        private readonly ITestOutputHelper output;
+
+        public NTreeTest(ITestOutputHelper testOutputHelper)
+        {
+            output = testOutputHelper;
+        }
+
+        [Fact]
+        public void CreateTree()
+        {
+            NTree<int> tree = new NTree<int>(0);
+            Assert.True(tree.CurrentData == 0);
+        }
+
+        [Fact]
+        public void AddNode()
+        {
+            NTree<int> tree = new NTree<int>(0);
+            var node1 = tree.AddNode(1);
+            var node2 = tree.AddNode(2);
+            Assert.True(node1.data == 1);
+            Assert.True(node1.parent.ToArray()[0].data == 0);
+            Assert.True(node2.data == 2);
+            Assert.True(node2.parent.ToArray()[0].data == 0);
+            Assert.True(tree.GetChild.Count == 2);
+        }
+
+        [Fact]
+        public void GetAllPath()
+        {
+            NTree<int> tree = new NTree<int>(0);
+            tree.AddNode(1);
+            tree.AddNode(2);
+            var p = tree.GetAllPath();
+        }
+
+        [Fact]
+        public void GetAllPathData()
+        {
+            NTree<int> tree = new NTree<int>(0);
+            tree.AddNode(1);
+            tree.AddNode(2);
+            tree.NextNode(1);
+            tree.AddNode(3);
+            tree.AddNode(4);
+            var p = tree.GetAllPathData();
+            foreach (List<int> l in p)
+            {
+                foreach(var i in l)
+                    output.WriteLine("{0}",i);
+                output.WriteLine("");
+               // Console.WriteLine();
+            }
+        }
+
+        [Fact]
+        public void PrevNode()
+        {
+            NTree<int> tree = new NTree<int>(0);
+            tree.AddNode(1);
+            tree.AddNode(2);
+            tree.NextNode(1);
+            tree.AddNode(3);
+            tree.AddNode(4, true);
+            Assert.True(tree.PrevNode(1).data == 1);
+            Assert.True(tree.PrevNode(0).data == 0);
+            Assert.True(tree.PrevNode(0) == null);
+        }
+
+
+
+        [Fact]
+        public void GetAllPathDataWithAddHead()
+        {
+            NTree<int> tree = new NTree<int>();
+            tree.AddHead(0, true);
+            tree.AddNode(1);
+            tree.AddNode(2);
+            tree.NextNode(1);
+            tree.AddNode(3);
+            tree.AddNode(4);
+            var p = tree.GetAllPathData();
+            foreach (List<int> l in p)
+            {
+                foreach (var i in l)
+                    output.WriteLine("{0}", i);
+                output.WriteLine("");
+                // Console.WriteLine();
+            }
+        }
+
+        [Fact]
+        public void GetAllPathDataWithMultipleHead()
+        {
+            NTree<string> tree = new NTree<string>();
+            tree.AddHead("a0");
+            tree.AddHead("a1");
+            tree.FindNode("a0");
+            tree.AddNode("b0");
+            var b1 = tree.AddNode("b1");
+            tree.FindNode("a1");
+            tree.AddNode("b2");
+            tree.AddNode(b1);
+            tree.AddNode(b1);
+            tree.AddNode(b1);
+            tree.AddNode(b1);
+            tree.FindNode("b0");
+            tree.AddNode("c0");
+            tree.FindNode("b1");
+            tree.AddNode("c1");
+            tree.AddNode("c2");
+            tree.FindNode("b2");
+            tree.AddNode("c3");
+            var p = tree.GetAllPathData();
+            foreach (List<string> l in p)
+            {
+                foreach (var i in l)
+                    output.WriteLine("{0}", i);
+                output.WriteLine("");
+                // Console.WriteLine();
+            }
+        }
+
+
+        [Fact]
+        public void CombineFalse()
+        {
+            NTree<string> tree = new NTree<string>();
+            tree.AddHead("A", true);
+            tree.AddNode("B1");
+            tree.AddNode("B2");
+            tree.FindNode("B1");
+            tree.AddNode("C1", true);
+            var D1 = tree.AddNode("D1");
+            tree.FindNode("B2");
+            tree.AddNode("C2", true);
+            tree.AddNode(D1, true);
+            tree.AddNode("E1", true);
+            tree.AddNode("F1", true);
+            tree.AddNode("G1");
+            tree.FindNode("E1");
+            tree.AddNode("F2");
+
+            NTree<string> chain = new NTree<string>();
+            chain.AddHead("X1", true);
+            chain.AddNode("X2");
+            chain.AddNode("X3");
+
+            Assert.False(tree.Combine(chain));
+            var p = tree.GetAllPathData();
+            foreach (List<string> l in p)
+            {
+                foreach (var i in l)
+                    output.WriteLine("{0}", i);
+                output.WriteLine("");
+                // Console.WriteLine();
+            }
+        }
+
+        [Fact]
+        public void CombineTrueShareHead()
+        {
+            NTree<string> tree = new NTree<string>();
+            tree.AddHead("A", true);
+            tree.AddNode("B1");
+            tree.AddNode("B2");
+            tree.FindNode("B1");
+            tree.AddNode("C1", true);
+            var D1 = tree.AddNode("D1");
+            tree.FindNode("B2");
+            tree.AddNode("C2", true);
+            tree.AddNode(D1, true);
+            tree.AddNode("E1", true);
+            tree.AddNode("F1", true);
+            tree.AddNode("G1");
+            tree.FindNode("E1");
+            tree.AddNode("F2");
+
+            output.WriteLine("********Init********");
+
+            var p = tree.GetAllPathData();
+            foreach (List<string> l in p)
+            {
+                foreach (var i in l)
+                    output.WriteLine("{0}", i);
+                output.WriteLine("");
+                // Console.WriteLine();
+            }
+
+            output.WriteLine("********Chain********");
+            NTree<string> chain = new NTree<string>();
+            chain.AddHead("A", true);
+            chain.AddNode("B1", true);
+            chain.AddNode("C1", true);
+            chain.AddNode("X1", true);
+            chain.AddNode("X2", true);
+            chain.AddNode("X3", true);
+            chain.AddNode("F1", true);
+            chain.AddNode("X4", true);
+
+            p = chain.GetAllPathData();
+            foreach (List<string> l in p)
+            {
+                foreach (var i in l)
+                    output.WriteLine("{0}", i);
+                output.WriteLine("");
+                // Console.WriteLine();
+            }
+            Assert.True(tree.Combine(chain));
+
+            output.WriteLine("********Result********");
+
+            p = tree.GetAllPathData();
+            foreach (List<string> l in p)
+            {
+                foreach (var i in l)
+                    output.WriteLine("{0}", i);
+                output.WriteLine("");
+                // Console.WriteLine();
+            }
+        }
+
+        [Fact]
+        public void CombineTrueNotSharingHead()
+        {
+            NTree<string> tree = new NTree<string>();
+            tree.AddHead("A", true);
+            tree.AddNode("B1");
+            tree.AddNode("B2");
+            tree.FindNode("B1");
+            tree.AddNode("C1", true);
+            var D1 = tree.AddNode("D1");
+            tree.FindNode("B2");
+            tree.AddNode("C2", true);
+            tree.AddNode(D1, true);
+            tree.AddNode("E1", true);
+            tree.AddNode("F1", true);
+            tree.AddNode("G1");
+            tree.FindNode("E1");
+            tree.AddNode("F2");
+
+            output.WriteLine("********Init********");
+
+            var p = tree.GetAllPathData();
+            foreach (List<string> l in p)
+            {
+                foreach (var i in l)
+                    output.WriteLine("{0}", i);
+                output.WriteLine("");
+                // Console.WriteLine();
+            }
+
+            output.WriteLine("********Chain********");
+            NTree<string> chain = new NTree<string>();
+            //chain.AddHead("A", true);
+            //chain.AddNode("B1", true);
+            //chain.AddNode("C1", true);
+            chain.AddHead("X1", true);
+            chain.AddNode("X2", true);
+            chain.AddNode("X3", true);
+            chain.AddNode("F1", true);
+            chain.AddNode("X4", true);
+
+            p = chain.GetAllPathData();
+            foreach (List<string> l in p)
+            {
+                foreach (var i in l)
+                    output.WriteLine("{0}", i);
+                output.WriteLine("");
+                // Console.WriteLine();
+            }
+            Assert.True(tree.Combine(chain));
+
+            output.WriteLine("********Result********");
+
+            p = tree.GetAllPathData();
+            foreach (List<string> l in p)
+            {
+                foreach (var i in l)
+                    output.WriteLine("{0}", i);
+                output.WriteLine("");
+                // Console.WriteLine();
+            }
+        }
+
+        [Fact]
+        public void CombineTrueNoCommon()
+        {
+            NTree<string> tree = new NTree<string>();
+            tree.AddHead("A", true);
+            tree.AddNode("B1");
+            tree.AddNode("B2");
+            tree.FindNode("B1");
+            tree.AddNode("C1", true);
+            var D1 = tree.AddNode("D1");
+            tree.FindNode("B2");
+            tree.AddNode("C2", true);
+            tree.AddNode(D1, true);
+            tree.AddNode("E1", true);
+            tree.AddNode("F1", true);
+            tree.AddNode("G1");
+            tree.FindNode("E1");
+            tree.AddNode("F2");
+
+            output.WriteLine("********Init********");
+
+            var p = tree.GetAllPathData();
+            foreach (List<string> l in p)
+            {
+                foreach (var i in l)
+                    output.WriteLine("{0}", i);
+                output.WriteLine("");
+                // Console.WriteLine();
+            }
+
+            output.WriteLine("********Chain********");
+            NTree<string> chain = new NTree<string>();
+            //chain.AddHead("A", true);
+            //chain.AddNode("B1", true);
+            //chain.AddNode("C1", true);
+            chain.AddHead("X1", true);
+            chain.AddNode("X2", true);
+            chain.AddNode("X3", true);
+            chain.AddNode("X4", true);
+
+            p = chain.GetAllPathData();
+            foreach (List<string> l in p)
+            {
+                foreach (var i in l)
+                    output.WriteLine("{0}", i);
+                output.WriteLine("");
+                // Console.WriteLine();
+            }
+            Assert.True(tree.Combine(chain));
+
+            output.WriteLine("********Result********");
+
+            p = tree.GetAllPathData();
+            foreach (List<string> l in p)
+            {
+                foreach (var i in l)
+                    output.WriteLine("{0}", i);
+                output.WriteLine("");
+                // Console.WriteLine();
+            }
+        }
+
+        [Fact]
+        public void CombineTrueNoCommonAndAddOne()
+        {
+            NTree<string> tree = new NTree<string>();
+            tree.AddHead("A", true);
+            tree.AddNode("B1");
+            tree.AddNode("B2");
+            tree.FindNode("B1");
+            tree.AddNode("C1", true);
+            var D1 = tree.AddNode("D1");
+            tree.FindNode("B2");
+            tree.AddNode("C2", true);
+            tree.AddNode(D1, true);
+            tree.AddNode("E1", true);
+            tree.AddNode("F1", true);
+            tree.AddNode("G1");
+            tree.FindNode("E1");
+            tree.AddNode("F2");
+
+            output.WriteLine("********Init********");
+
+            var p = tree.GetAllPathData();
+            foreach (List<string> l in p)
+            {
+                foreach (var i in l)
+                    output.WriteLine("{0}", i);
+                output.WriteLine("");
+                // Console.WriteLine();
+            }
+
+            output.WriteLine("********Chain1********");
+            NTree<string> chain = new NTree<string>();
+            //chain.AddHead("A", true);
+            //chain.AddNode("B1", true);
+            //chain.AddNode("C1", true);
+            chain.AddHead("X1", true);
+            chain.AddNode("X2", true);
+            chain.AddNode("X3", true);
+            chain.AddNode("X4", true);
+
+            p = chain.GetAllPathData();
+            foreach (List<string> l in p)
+            {
+                foreach (var i in l)
+                    output.WriteLine("{0}", i);
+                output.WriteLine("");
+                // Console.WriteLine();
+            }
+            Assert.True(tree.Combine(chain));
+
+            output.WriteLine("********Chain2********");
+            chain = new NTree<string>();
+            //chain.AddHead("A", true);
+            //chain.AddNode("B1", true);
+            //chain.AddNode("C1", true);
+            chain.AddHead("X3", true);
+            chain.AddNode("F1", true);
+            chain.AddNode("X4", true);
+
+            p = chain.GetAllPathData();
+            foreach (List<string> l in p)
+            {
+                foreach (var i in l)
+                    output.WriteLine("{0}", i);
+                output.WriteLine("");
+                // Console.WriteLine();
+            }
+            Assert.True(tree.Combine(chain));
+
+
+            output.WriteLine("********Result********");
+
+            p = tree.GetAllPathData();
+            foreach (List<string> l in p)
+            {
+                foreach (var i in l)
+                    output.WriteLine("{0}", i);
+                output.WriteLine("");
+                // Console.WriteLine();
+            }
+        }
+    }
+}
--- SubwayConnect/lib/NTree.cs
+++ SubwayConnect/lib/NTree.cs
@@ -1,92 +1,206 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace MetroMate
 {
     public class NTreeNode<T>
     {
         public T data;
-        public List<NTreeNode<T>> child { get; set; }
-        public NTreeNode<T> parent { get; set; }
-        public NTreeNode(T data, NTreeNode<T> parent, List<NTreeNode<T>> child = null)
+        public HashSet<NTreeNode<T>> child { get; set; }
+        public HashSet<NTreeNode<T>> parent { get; set; }
+        public NTreeNode(T data)
+        {
+            this.data = data;
+            this.parent = new HashSet<NTreeNode<T>>();
+            this.child = new HashSet<NTreeNode<T>>();
+        }
+        public NTreeNode(T data, NTreeNode<T> parent, HashSet<NTreeNode<T>> child = null)
+        {
+            this.data = data;
+            this.parent = new HashSet<NTreeNode<T>>();
+            this.parent.Add(parent);
+            this.child = child;
+            if (this.child == null)
+                this.child = new HashSet<NTreeNode<T>>();
+        }
+        public NTreeNode(T data, HashSet<NTreeNode<T>> parent, HashSet<NTreeNode<T>> child = null)
         {
             this.data = data;
             this.parent = parent;
             this.child = child;
             if (this.child == null)
-                this.child = new List<NTreeNode<T>>();
+                this.child = new HashSet<NTreeNode<T>>();
         }
-
     };
 
     public class NTree<T>
     {
-        private NTreeNode<T> Head;
+        private List<NTreeNode<T>> Head;
         private NTreeNode<T> Current;
+        private Dictionary<T, NTreeNode<T>> Nodes;
 
         public T CurrentData { get => Current.data; set => Current.data = value; }
         public NTreeNode<T> GetCurrentNode { get { return Current; } }
-        public List<NTreeNode<T>> GetChild { get => Current.child; }
+        public HashSet<NTreeNode<T>> GetChild { get => Current.child; }
+        public NTreeNode<T> GetNode(T data) {
+            if (Nodes.ContainsKey(data)) return Nodes[data];
+            return null;
+        }
 
+        // Constructor
+        // Default
+        // With Only Data
+        // With Existing Node
         public NTree()
         {
-            Head = null;
+            Head = new List<NTreeNode<T>>();
             Current = null;
+            Nodes = new Dictionary<T, NTreeNode<T>>();
         }
-
         public NTree(T head)
         {
-            Head = new NTreeNode<T>(head, null);
-            Current = Head;
+            Head = new List<NTreeNode<T>>();
+            Head.Add(new NTreeNode<T>(head));
+            Current = Head[0];
+            Nodes = new Dictionary<T, NTreeNode<T>>();
         }
-
         public NTree(NTreeNode<T> node)
         {
-            Head = node;
-            Head.parent = null;
-            Current = Head;
+            Head = new List<NTreeNode<T>>();
+            Head.Add(new NTreeNode<T>(node.data, new HashSet<NTreeNode<T>>(), node.child));
+            Current = Head[0];
+            Nodes = new Dictionary<T, NTreeNode<T>>();
         }
 
-        public void Reset() { Head = Current; }
+        public void Reset() { Current = Head[0]; }
 
-        public NTreeNode<T> AddNode(T child)
+        // Add Head Node
+        public NTreeNode<T> AddHead(T data, bool move = false)
         {
+            if (Nodes.ContainsKey(data))
+                throw new System.Exception("Element is already exist.");
+
+            NTreeNode<T> newNode = new NTreeNode<T>(data, new HashSet<NTreeNode<T>>());
+            Head.Add(newNode);
+            Nodes[data] = newNode;
+            if (move) Current = newNode;
+            return newNode;
+        }
+        public NTreeNode<T> AddHead(NTreeNode<T> n, bool move = false)
+        {
+            if (Nodes.ContainsKey(n.data))
+                throw new System.Exception("Element is already exist.");
+
+            NTreeNode<T> newNode = new NTreeNode<T>(n.data, new HashSet<NTreeNode<T>>(), n.child);
+            Head.Add(newNode);
+            Nodes[n.data] = newNode;
+            if (move) Current = newNode;
+            return newNode;
+        }
+
+
+        // Add Leaf Node
+        public NTreeNode<T> AddNode(T child, bool move = false)
+        {
+            if (Nodes.ContainsKey(child))
+                throw new System.Exception("Element is already exist.");
+
             NTreeNode<T> newNode = new NTreeNode<T>(child, Current);
+            
             Current.child.Add(newNode);
+            if (move) Current = newNode;
+            Nodes[child] = newNode;
             return newNode;
         }
-
-        public NTreeNode<T> AddNode(NTreeNode<T> child)
+        public NTreeNode<T> AddNode(NTreeNode<T> child, bool move = false)
         {
-            NTreeNode<T> newNode = child;
-            newNode.parent = Current;
-            Current.child.Add(newNode);
+            NTreeNode<T> newNode;
+            if (Nodes.ContainsKey(child.data))
+                if (!ReferenceEquals(Nodes[child.data], child))
+                    throw new System.Exception("Element is already exist.");
+                else
+                {
+                    if (!Current.child.Contains(child))
+                        Current.child.Add(child);
+                    if (!child.parent.Contains(Current))
+                        child.parent.Add(Current);
+                    newNode = child;
+                }
+            else
+            {
+                newNode  = new NTreeNode<T>(child.data, Current, child.child);
+                Current.child.Add(newNode);
+            }
+
+            if (move) Current = newNode;
+            Nodes[child.data] = newNode;
             return newNode;
         }
 
+        // Move prev and next
         // will return null if not exsist
         public NTreeNode<T> NextNode(T node)
         {
             foreach (NTreeNode<T> childnode in Current.child)
-            {
                 if (childnode.data.Equals(node))
                 {
                     Current = childnode;
                     return Current;
                 }
-            }
+            
             return null;
         }
+        public NTreeNode<T> PrevNode(T node)
+        {
+            foreach (NTreeNode<T> parentnode in Current.parent)
+                if (parentnode.data.Equals(node))
+                {
+                    Current = parentnode;
+                    return Current;
+                }
+            return null;
+        }
+        public NTreeNode<T> FindNode(T node)
+        {
+            Current = GetNode(node);
+            return Current;
+        }
 
+        // Get all possible path
+        // return type list of Node traces
+        public List< List<NTreeNode<T>> > GetAllPath()
+        {
+            List<List<NTreeNode<T>>> r = new List<List<NTreeNode<T>>>();
+            foreach (var h in Head)
+                r.AddRange(GetAllPathHelper(h, new List<NTreeNode<T>>()));
+            return r;
+        }
+        public List<List<T>> GetAllPathData()
+        {
 
+            List<List<NTreeNode<T>>> r = new List<List<NTreeNode<T>>>();
+            foreach (var h in Head)
+                r.AddRange(GetAllPathHelper(h, new List<NTreeNode<T>>()));
+            List<List<T>> result = new List<List<T>>();
+            foreach (var l in r)
+            {
+                result.Add(new List<T>());
+                foreach (var t in l)
+                    result[result.Count - 1].Add(t.data);
+            }
+            return result;
+        }
         private List<List<NTreeNode<T>>> GetAllPathHelper(NTreeNode<T> CurrentNode,
                                                           List<NTreeNode<T>> trace)
         {
             List<List<NTreeNode<T>>> r = new List<List<NTreeNode<T>>>();
             if (CurrentNode.child.Count == 0)
             {
-                List<NTreeNode<T>> newtrace = trace;
+                List<NTreeNode<T>> newtrace;
+                NTreeNode<T>[] tracearray = trace.ToArray();
+                newtrace = tracearray.OfType<NTreeNode<T>>().ToList();
                 newtrace.Add(CurrentNode);
                 r.Add(newtrace);
                 return r;
@@ -98,10 +212,32 @@ namespace MetroMate
             return r;
         }
 
-        // return type list of Node traces
-        public List< List<NTreeNode<T>> > GetAllPath()
+        public bool Combine(NTree<T> chain)
         {
-            return GetAllPathHelper(Head, new List<NTreeNode<T>>());
+            Reset();
+            var t = chain.GetAllPath();
+            if (t.Count != 1) return false;
+            List<NTreeNode<T>> lchain = t[0];
+            int i = 0;
+            // Find the first common nodes
+            for (i = 0; i < lchain.Count; i++)
+            {
+                if (!Nodes.ContainsKey(lchain[i].data))
+                    if (i == 0)
+                        AddHead(lchain[i].data, true);
+                    else
+                        AddNode(lchain[i].data, true);
+                else if (i == 0)
+                    FindNode(lchain[i].data);
+                else
+                {
+                    NTreeNode<T> n = GetNode(lchain[i].data);
+                    AddNode(n, true);
+                }
+
+            }
+            Reset();
+            return true;
         }
     };
 }
